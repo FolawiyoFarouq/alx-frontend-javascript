85dc21d442790ff3468399bd19fac37a63f1118f
FolawiyoFarouq <folawiyofarouq@gmail.com>
2024-09-17 17:15:22 +0000

    Task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,7 @@
-import getListStudents from './0-get_list_students';
-
-console.log(getListStudents());
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
